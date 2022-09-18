339eb16ad7aa99e784496a16d8eb50146242df8b
ignoxx <ignoxx@yahoo.de>
2022-09-18 23:49:19 +0200

    Init

---
+++ 2021/01/go.mod
@@ -0,0 +1,3 @@
+module github.com/ignoxx/aoc/01
+
+go 1.19
---
+++ 2021/01/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func get_input() string {
+	content, err := os.ReadFile("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(content)
+}
+
+func main() {
+	input := strings.Fields(get_input())
+
+	prevDepth := 0
+	measurementCount := 0
+
+	for index, value := range input {
+		depth, err := strconv.Atoi(value)
+
+		if err != nil {
+			log.Fatal("failed to parse input!! " + err.Error())
+		}
+
+		if index == 0 {
+			prevDepth = depth
+			continue
+		}
+
+		if depth > prevDepth {
+			measurementCount++
+		}
+
+		prevDepth = depth
+	}
+
+	fmt.Println("there are %v measurements that are larger than the previous measurement.", measurementCount)
+}
